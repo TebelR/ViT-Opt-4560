9b8007727babbed13198e14d79477071e7e5f13e
Roman Tebel <tebelr@myumanitoba.ca>
2025-02-27 00:11:41 -0600

    Equalizing class representation in the ViT data

--- scripts/ImageMagicSampling.py
+++ scripts/ImageMagicSampling.py
@@ -24,13 +24,13 @@ print(needed_images)
 
 
 
-# duplicate images
-for cls, needed in needed_images.items():
-    class_images = [f for f in os.listdir(dataset_path) if f.endswith(f"_{cls}.jpg")]
-    
-    for i in range(needed):
-        original_img = class_images[i % len(class_images)] 
-        new_img_name = f"changed{i+10000}_{cls}.jpg"
-        shutil.copy(os.path.join(dataset_path, original_img), os.path.join(dataset_path, new_img_name))
+# # duplicate images
+# for cls, needed in needed_images.items():
+#     class_path = os.path.join(dataset_path, cls)
+#     for image_name in os.listdir(class_path):  
+#         for i in range(needed):
+#             original_img = image_name
+#             new_img_name = f"changed{i+10000}_{cls}.jpg"
+#             shutil.copy(os.path.join(class_path, original_img), os.path.join(class_path, new_img_name))
 
-print("Dataset balancing complete!")
+# print("Dataset balancing complete!")
